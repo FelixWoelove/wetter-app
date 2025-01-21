e5f3f5e340cb26c22a172be30d7a89e29f9f5669
Felix Kalske <felix.kalske@dci-student.org>
2025-01-21 15:49:18 +0100

    feierabend commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -30,6 +30,8 @@ const Weather = ({ city, language }) => {
     fetchWeather();
   }, [city, language]);
 
+  const wetter = weather?.weather?.[0];
+
   return (
     <div className="weather-container">
       {error && <p>{error}</p>}
@@ -49,9 +51,52 @@ const Weather = ({ city, language }) => {
             alt="weather icon"
           /> */}
           <div className="iconContainer">
-            <div className="partly_cloudy">
-              <div className="partly_cloudy__sun"></div> <div className="partly_cloudy__cloud"></div>
-            </div>
+            {wetter.icon === "01d" || wetter.icon === "01n" ? (
+              <div className="sunny"></div>
+            ) : null}
+            {wetter.icon === "02d" || wetter.icon === "02n" ? (
+              <div class="partly_cloudy">
+                <div class="partly_cloudy__sun"></div>
+                <div class="partly_cloudy__cloud"></div>
+              </div>
+            ) : null}
+            {wetter.icon === "03d" || wetter.icon === "03n" ? (
+              <div className="cloudy"></div>
+            ) : null}
+            {wetter.icon === "04d" || wetter.icon === "04n" ? (
+              <div className="cloudy"></div>
+            ) : null}
+            {wetter.icon === "09d" || wetter.icon === "09n" ? (
+              <div className="rainy">
+                <div class="rainy__cloud"></div>
+                <div class="rainy__rain"></div>
+              </div>
+            ) : null}
+            {wetter.icon === "10d" || wetter.icon === "10n" ? (
+              <div className="rainy">
+                <div class="rainy__cloud"></div>
+                <div class="rainy__rain"></div>
+              </div>
+            ) : null}
+            {wetter.icon === "11d" || wetter.icon === "11n" ? (
+              <div class="thundery">
+                <div class="thundery__cloud"></div>
+                <div class="thundery__rain"></div>
+              </div>
+            ) : null}
+
+            {wetter.icon === "13d" || wetter.icon === "13n" ? (
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                alt="weather icon"
+              />
+            ) : null}
+            {wetter.icon === "50d" || wetter.icon === "50n" ? (
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                alt="weather icon"
+              />
+            ) : null}
           </div>
         </>
       ) : (
@@ -68,8 +113,3 @@ const Weather = ({ city, language }) => {
 };
 
 export default Weather;
-
-
-
-
-
